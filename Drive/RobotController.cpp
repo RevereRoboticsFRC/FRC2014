88e8d3404e2e7b4707730532ba6d705cc9c0c687
Vince <vincentzhang1996@gmail.com>
2014-01-17 15:24:00 -0500

    Implemented Cartesian based driving
    Completed polar based driving code, commented out.
    Cartesian (joystick X and Y) tank drive implemented.

--- Drive/RobotController.cpp
+++ Drive/RobotController.cpp
@@ -1,5 +1,6 @@
 #include "RobotController.h"
 #include "../Utils/RobotMath.h"
+#include <cmath>
 
 RobotController::RobotController(Driver* d, Joystick* j) {
 	driver = d;
@@ -7,18 +8,37 @@ RobotController::RobotController(Driver* d, Joystick* j) {
 }
 
 void RobotController::TeleopTick() {
-	//	Magnitude varies between 0.0 and 1.0, but due to joystick error may exceed 1.0
-	float magnitude = Clamp(0.0, 1.0, joystick->GetMagnitude());
-	//	Direction varies between +/-0.0 and +/-180 degrees: -90 is left, +90 right.
-	float directionDeg = joystick->GetDirectionDegrees();
-
-	//	TODO: Input curve mapping and tank drive logic/math goes here
+//	//	Polar method
+//	//	Magnitude varies between 0.0 and 1.0, but due to joystick error may exceed 1.0
+//	float magnitude = Clamp(0.0, 1.0, joystick->GetMagnitude());
+//	//	Direction varies between +/-0.0 and +/-180 degrees: -90 is left, +90 right.
+//	float directionDeg = joystick->GetDirectionDegrees();
+//	//	TODO: Input curve mapping
+//	
+//	//	Invert magnitude if y is negative (past +/-90 towards +/-180)
+//	if(directionDeg > 90.0 || directionDeg < -90.0) {
+//		magnitude = -magnitude;
+//	}
+//	driver->Drive(magnitude, magnitude);
 	
-	//	Test for now: Drive straight
-	//	Invert magnitude if y is negative (past +/-90 towards +/-180)
-	if(directionDeg > 90.0 || directionDeg < -90.0) {
-		magnitude = -magnitude;
-	}
-	driver->Drive(magnitude, magnitude);
+	//	Cartesian method
+	float joyX = Clamp(-1.0, 1.0, joystick->GetX(Joystick::kRightHand));
+	float joyY = Clamp(-1.0, 1.0, joystick->GetY(Joystick::kRightHand));
+	float magnitude = Clamp(0.0, 1.0, joystick->GetMagnitude());
+	
+	//	TODO: Input curve mapping
+	
+	
+	//	And tank drive logic/math goes here
+	float u = (1 - abs(joyX)) * joyY + joyY;
+	float v = (1 - abs(joyY)) * joyX + joyX;
+	float right = (u + v)/2.0;
+	float left  = (u - v)/2.0;
+	
+	//	Multiply the output values by magnitude to get the resultant
+	right *= magnitude;
+	left *= magnitude;
+	
+	driver->Drive(left, right);
 }
 
