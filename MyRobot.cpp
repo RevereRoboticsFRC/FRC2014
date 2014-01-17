11bf2f7375990691828aaa98012fc7cd4acbde72
Vince <vincentzhang1996@gmail.com>
2014-01-17 16:34:41 -0500

    Implemented Ticking System
    Utilize a ticking system to track time and allow for tick-value specific
    operations that are guaranteed to execute.
    Updated Test mode to utilize Ticks.
    Updated xxxInit() and xxxPeriodic() to use Ticks.
    Watchdog feeding is now a per-tick operation.

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -1,11 +1,12 @@
 #include "WPILib.h"
 #include "Drive/RobotController.h"
 #include "Utils/Logger.h"
+#include "Utils/RobotMath.h"
 #include "Drive/Driver.h"
 
 #define VERSION "1.0 "
 #define TEAM_NUMBER "Team 5249"
-
+#define TICK_LENGTH 20.0
 
 //Test comment
 
@@ -17,13 +18,16 @@ class RevereRobot: public IterativeRobot {
 	//	Sanity timer
 	Timer* timer;
 
+	unsigned int modeTickCount;
+
 public:
 	RevereRobot() {
 		logger = new Logger(FINE, "RevereBot");
 		driver = new Driver(1, 2);
 		controller = new RobotController(driver, new Joystick(1));
 		timer = new Timer();
-		
+		ResetTick();
+
 		//	Set update period to sync with robot control packets (20ms nominal)
 		SetPeriod(0);
 	}
@@ -39,7 +43,6 @@ public:
 		logger->All(__DATE__);
 		logger->All(__TIME__);
 		logger->All("=========================");
-
 		logger->Info("Finished global init.");
 	}
 
@@ -51,6 +54,7 @@ public:
 		driver->Stop();
 		driver->SetDisabled(true);
 		driver->SetSafetyEnabled(true);
+		ResetTick();
 		logger->Info("Finished disabled mode init.");
 	}
 
@@ -59,7 +63,7 @@ public:
 	 * Do nothing but feed the doggy.
 	 */
 	void RevereRobot::DisabledPeriodic() {
-		m_watchdog.Feed();
+		OnTick();
 	}
 
 	/**
@@ -70,9 +74,7 @@ public:
 		driver->Stop();
 		driver->SetDisabled(false);
 		driver->SetSafetyEnabled(false);
-		timer->Stop();
-		timer->Reset();
-		timer->Start();
+		ResetTick();
 		logger->Info("Finished auton mode init.");
 	}
 
@@ -80,12 +82,12 @@ public:
 	 * Periodic code for autonomous mode.
 	 */
 	void RevereRobot::AutonomousPeriodic() {
+		OnTick();
 		//For testing, allowing disable autonomous after one second
 		//	Timer takes seconds as the argument
 		if (timer->HasPeriodPassed(1.0)) {
 			driver->SetDisabled(true);
 		}
-		m_watchdog.Feed();
 		//Drive forward at one quarter speed 
 		driver->Drive(0.25, 0.25);
 	}
@@ -99,6 +101,7 @@ public:
 		driver->SetDisabled(false);
 		driver->SetSafetyEnabled(true);
 		driver->SetExpiration(0.5);
+		ResetTick();
 		logger->Info("Finished teleop mode init.");
 	}
 
@@ -106,7 +109,7 @@ public:
 	 * Periodic code for teleop mode.
 	 */
 	void RevereRobot::TeleopPeriodic() {
-		m_watchdog.Feed();
+		OnTick();
 		controller->TeleopTick();
 	}
 
@@ -119,9 +122,7 @@ public:
 		driver->SetDisabled(false);
 		driver->SetSafetyEnabled(true);
 		driver->SetExpiration(0.5);
-		timer->Stop();
-		timer->Reset();
-		timer->Start();
+		ResetTick();
 		logger->Info("Finished test mode init.");
 	}
 
@@ -129,33 +130,72 @@ public:
 	 * Periodic code for test mode.
 	 */
 	void RevereRobot::TestPeriodic() {
+		OnTick();
 		//	Temporary: Test motors same speed for 1 second
-		if(!timer->HasPeriodPassed(1.0)) {
+		if (IsInRange(0.0, 1.0, TicksToSeconds(modeTickCount))) {
 			driver->Drive(0.25, 0.25);
 		}
 		//	Stop for 1 second
-		else if(!timer->HasPeriodPassed(2.0)) {
+		if (IsInRange(1.0, 2.0, TicksToSeconds(modeTickCount))) {
 			driver->Stop();
 		}
 		//	Then go backwards 1 second
-		else if(!timer->HasPeriodPassed(3.0)) {
+		else if (IsInRange(2.0, 3.0, TicksToSeconds(modeTickCount))) {
 			driver->Drive(-0.25, -0.25);
 		}
 		//	Then stop for another second
-		else if(!timer->HasPeriodPassed(4.0)){
+		else if (IsInRange(4.0, 5.0, TicksToSeconds(modeTickCount))) {
 			driver->Stop();
 		}
 		//	Teleop
 		else {
 			TeleopPeriodic();
 		}
-		
+
+	}
+
+	/**
+	 * Updates the tick system. Should be called by all xxxPeriodic() methods.
+	 */
+	void RevereRobot::OnTick() {
+		//	We don't care about overflow behavior because it would take 4,294,967,295 ticks of 20ms 
+		//  each to overflow, giving us 85,899,345.9 seconds (or 23,860 hours, which is 994 days, or 3 years)
+		//	of continuous operation per mode.
+		//	16 bits would only give 65,535 ticks, or about 20 minutes of operation. 
+		modeTickCount++;
+
+		m_watchdog.Feed();
+	}
+
+	unsigned int RevereRobot::SecondsToTicks(float seconds) {
+		//	Ensure seconds is non-negative and flip sign if that's the case (should we really do this?)
+		if(seconds < 0.0) {
+			seconds *= -1.0;
+		}
+		//	   seconds->millis->ticks
+		return (unsigned int)(seconds * 1000.0 / TICK_LENGTH);
+	}
+
+	float RevereRobot::TicksToSeconds(unsigned int ticks) {
+		//	Ticks will always be negative! Yay!
+		//	   ticks->millis->seconds
+		return ticks * TICK_LENGTH / 1000.0;
+	}
+
+	/**
+	 * Resets the tick system for the mode change. Should be called by all xxxInit() methods.
+	 */
+	void RevereRobot::ResetTick() {
+		modeTickCount = 0;
+		timer->Stop();
+		timer->Reset();
+		timer->Start();
 	}
 
 	/**
 	 * Destructor
 	 */
-	~RevereRobot() {
+	RevereRobot::~RevereRobot() {
 		logger->All("Main Destructor");
 		delete controller;
 		controller = NULL;
